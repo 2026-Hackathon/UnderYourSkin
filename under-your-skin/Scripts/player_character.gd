8b8c17ad6d9c2ca0628c969c8dd5eb95dd1d9240
GeoGeov <gggeorgi09@gmail.com>
2026-03-03 18:46:13 +0000

    Added Mid Air Jump Limit

--- under-your-skin/Scripts/player_character.gd
+++ under-your-skin/Scripts/player_character.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var fling_power: float = 80.0
 @export var air_resistance: float = 0.02
 @export var max_drag_distance: float = 100.0
+@export var air_jumps_max: int = 1
+var air_jumps_count: int = 2 #Dummy Val, Replaced when intialized
 
 @onready var tilemap_layer: TileMapLayer = get_tree().get_first_node_in_group("ground")
 
@@ -12,11 +14,11 @@ var drag_start: Vector2
 var body_scale_original: Vector2
 var current_friction: float = 1.0
 
-#Initilaize Player Size
-#Needed so when scale is changed player remains Visible
+#Initilaize some Vals
+#Initilaize Player Size Needed so when scale is changed player remains Visible
 func _ready():
 	body_scale_original = scale  
-	
+	air_jumps_count = air_jumps_max
 #Key Input
 func _input(event: InputEvent):
 	#If Input Is Mouse
@@ -26,7 +28,7 @@ func _input(event: InputEvent):
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			
 			#On Press Get Starting Position
-			if event.pressed:
+			if event.pressed  and air_jumps_count > 0:
 				drag_start = get_global_mouse_position()
 				is_dragging = true
 				#Increase Player Size for Visual Cue
@@ -39,26 +41,32 @@ func _input(event: InputEvent):
 					var drag_end = get_global_mouse_position()
 					var drag_vec = (drag_end - drag_start).limit_length(max_drag_distance)
 					velocity = -drag_vec.normalized() * drag_vec.length() / 10.0 * fling_power
-					
+					#End Drag
 					is_dragging = false
 					#On release Size back to Normal
 					scale = body_scale_original
+					#Decrease Jump Count
+					air_jumps_count -= 1
 
 func _physics_process(delta: float):
-	# Gravity
+	#Apply Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		air_jumps_count = air_jumps_max
 
-	# Friction/resistance on x velocity only
+	# Friction/ Air resistance
+	# Friciton only applys on Ground, Air Resistance always
 	if is_on_floor():
 		current_friction = get_tile_friction()
 		velocity.x = move_toward(velocity.x, 0.0, (1.0 / current_friction) * 300.0 * delta)
-	else:
-		velocity.x *= (1.0 - air_resistance)
+	velocity *= (1.0 - air_resistance)
 
+	#Godot Built in Move Larp
 	move_and_slide()
 
 #Helper Function: Gets Friction of current Tile
+#Assumes 1.0 if not present.
 func get_tile_friction() -> float:
 	if tilemap_layer == null:
 		return 1.0
@@ -66,7 +74,7 @@ func get_tile_friction() -> float:
 	# Get collision position (foot position)
 	var foot_pos = global_position + Vector2(0, 20)
 	
-	# Find which tile we're standing on
+	# Finds which tile player is standing on
 	var tile_coords = tilemap_layer.local_to_map(tilemap_layer.to_local(foot_pos))
 	var tile_data = tilemap_layer.get_cell_tile_data(tile_coords)  # Fixed: only Vector2i coords
 	
