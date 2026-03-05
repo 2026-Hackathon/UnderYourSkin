ddf19305c47d94afb5bc43b9669ce4a216cf4bcb
GeoGeov <gggeorgi09@gmail.com>
2026-03-05 22:22:31 +0000

    Changed Scene Menu goes to

--- under-your-skin/Scripts/tempmenu.gd
+++ under-your-skin/Scripts/tempmenu.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://TempScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/TempScene.tscn")
