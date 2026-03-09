6b8368283af5d9d366b8b0aab3b4474e61bf7eba
Jake Burger <BurtonJake09@gmail.com>
2026-03-09 21:17:47 +0000

    Pause Screen greys background
    Black sprite is faded and comes in when paused

--- under-your-skin/Scripts/playing_ui.gd
+++ under-your-skin/Scripts/playing_ui.gd
@@ -15,12 +15,15 @@ func _process(_delta: float) -> void:
 func _on_pause_button_pressed() -> void:
 	#freeze the gameplay
 	Globals.is_frozen = not Globals.is_frozen
-	#un/freeze explosion animation
+	#CODE BELOW: 1. Finds whether frozen or not 2. Update all things [organ animation, pausebuttonsprite, pausescreen bg)
 	var organnode = get_node("/root/Node2D/PlayerCharacter/PlayerSprite/Organs")    
 	var pausebuttonspritenode = get_node("PauseButtonPadding/PauseButton/PauseSprite")
+	var pausescreenbgnode = get_node("PauseMenu/PauseScreenBgContainer/PauseScreenBg")
 	if Globals.is_frozen:
 		organnode.pause_explosion()
 		pausebuttonspritenode.texture = playsprite
+		pausescreenbgnode.show()
 	else:
 		organnode.resume_explosion()
 		pausebuttonspritenode.texture = pausesprite
+		pausescreenbgnode.hide()
