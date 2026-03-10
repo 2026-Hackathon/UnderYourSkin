f7d7bae6d2a57bc0fc07ad827aa61409bc61c4fb
Jake Burger <BurtonJake09@gmail.com>
2026-03-10 21:44:49 +0000

    Fixed Main Menu Zoom Slider
    Idek what i did but you can click it fr now

--- under-your-skin/Scripts/tempmenu.gd
+++ under-your-skin/Scripts/tempmenu.gd
@@ -45,4 +45,5 @@ func _on_movement_control_button_pressed() -> void:
 
 
 func _on_zoom_slider_drag_ended(value_changed: bool) -> void:
-	Globals.game_zoom = ($"OptionMenu/OptionMenuPadding/OptionList/ZoomControlContainer/Zoom Slider".value / 25) + 1
+	var slidernode = get_node("OptionMenu/OptionMenuPadding/OptionList/ZoomControlContainer/Zoom Slider")
+	Globals.game_zoom = (slidernode.value / 25) + 1
