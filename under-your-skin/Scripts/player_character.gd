5ded23ec0baf5fac0163fe47194a54146709db69
GeoGeov <gggeorgi09@gmail.com>
2026-03-11 21:29:14 +0000

    Player init with no jumps

--- under-your-skin/Scripts/player_character.gd
+++ under-your-skin/Scripts/player_character.gd
@@ -57,7 +57,7 @@ var is_on_sticky: bool = false
 #Initilaize Player Size Needed so when scale is changed player remains Visible
 func _ready():
 	original_scale = scale  
-	jump_count = max_jumps
+	jump_count = 0
 
 #General Physics Functions Below
 # Key Input
