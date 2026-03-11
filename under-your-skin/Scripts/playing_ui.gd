c4d00c8fb6a7ab3d0a22a319659558bd6bf439a0
Jake Burger <BurtonJake09@gmail.com>
2026-03-11 00:26:34 +0000

    UI
    i literally coded this 30s ago and already forgot. OH ITS THE SLIDER INITIALISER SO ZOOM STAYS OR SUM and also maybe sumthing else

--- under-your-skin/Scripts/playing_ui.gd
+++ under-your-skin/Scripts/playing_ui.gd
@@ -4,7 +4,7 @@ var pausesprite = preload("res://Sprites/Menu/PauseSpriteWithShading.png")
 var playsprite = preload("res://Sprites/Menu/play button.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$"PauseMenuList/ZoomControlContainer/Zoom Slider".value = (Globals.game_zoom -1) * 25
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
--- under-your-skin/Scripts/tempmenu.gd
+++ under-your-skin/Scripts/tempmenu.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	#Hides the other menus upin start
 	$OptionMenu.hide()
 	$BackButtonContainer.hide()
+	$"OptionMenu/OptionMenuPadding/OptionList/ZoomControlContainer/Zoom Slider".value = 25
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +16,7 @@ func _process(_delta: float) -> void:
 
 func _on_playbutton_pressed() -> void:
 	#play button opens level scene
-	get_tree().change_scene_to_file("res://Scenes/GameLevel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/TempScene.tscn")
 
 
 func _on_option_button_pressed() -> void:
