c161860ff7d43cc8ca6b30bf532baf47d4173153
Jake Burger <BurtonJake09@gmail.com>
2026-03-14 14:43:16 +0000

    Hides organelle upon load

--- under-your-skin/Scripts/organs.gd
+++ under-your-skin/Scripts/organs.gd
@@ -6,7 +6,7 @@ var explodetween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 # Called when explosion should occur i.e when double jumping
 func explosion():
