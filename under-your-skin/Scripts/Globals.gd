2b0f2e33dcfe893c2e774ed1a968061d75a73137
GeoGeov <gggeorgi09@gmail.com>
2026-03-14 16:38:44 +0000

    Added Jumping Sounds

--- under-your-skin/Scripts/Globals.gd
+++ under-your-skin/Scripts/Globals.gd
@@ -5,6 +5,7 @@ var current_time_scale: float = 1.0
 var is_frozen: bool = false
 var game_zoom: float = 2
 var lights_on: bool = true
+var sound_volume: float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
